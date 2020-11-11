55c037929b68388ff12e6cd47e9016b8de36a20d
kyli0x <kyli0x@protonmail.ch>
2020-11-11 16:41:23 -0500

    added yml file for urls

--- handlecheck.py
+++ handlecheck.py
@@ -1,59 +1,34 @@
 #!/usr/bin/env python3
 import os
+import yaml
 import requests
 
 # colors
 W  = '\033[0m'  # white (default)
 P  = '\033[1;35m' # purple
 C  = '\033[1;36m' # cyan
+    
 
-username = ''
+def checker():
+    handle = input(C+' Type ['+P+'exit'+C+'] to close the program\n \n Enter a username to search:\n \n > '+W)
+    if handle == 'exit':
+        exit()
+    else:
+        with open('websites.yml') as f:
+            data = yaml.safe_load(f)
+            links = data['sites']
 
-def checker(url):
-    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-    if r.status_code == 200:
-        return True
-    return False
+        weblinks = ["{}{}".format(i,handle) for i in links]
 
-def github(handle):
-    url = 'https://github.com/' + handle
-    return checker(url)
+        for url in weblinks:
+            r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+            if r.status_code == 200:
+                print(C+'Unavailable for: '+W+'' + url)
+            else:
+                print(C+'Available for:   '+W+'' + url)
+        main()
 
-def instagram(handle):
-    url = 'https://www.instagram.com/' + handle
-    return checker(url)
 
-def medium(handle):
-    url = 'https://medium.com/@' + handle
-    return checker(url)
-
-def minecraft(handle):
-    url = 'https://www.mc-heads.net/minecraft/profile/' + handle
-    return checker(url)
-
-def pastebin(handle):
-    url = 'https://pastebin.com/u/' + handle
-    return checker(url)
-
-def reddit(handle):
-    url = 'https://reddit.com/user/' + handle
-    return checker(url)
-
-def tryhackme(handle):
-    url = 'https://tryhackme.com/p/' + handle
-    return checker(url)
-
-def twitch(handle):
-    url = 'https://twitch.tv/' + handle
-    return checker(url)
-
-def twitter(handle):
-    url = 'https://www.twitter.com/' + handle
-    return checker(url)
-
-def youtube(handle):
-    url = 'https://www.youtube.com/user/' + handle
-    return checker(url)
 
 def banner():
     try:
@@ -84,23 +59,7 @@ def banner():
     print ('')
 
 def main():
-    username = input(C+' Type ['+P+'exit'+C+'] to close the program\n \n Enter a username to search:\n \n > '+W)
-    if username == 'exit':
-        exit()
-    else:
-        print('')
-        print(W+'Github:    '+C+'' + (''+P+'Unavailable' if github(username) else ''+P+'Available'))
-        print(W+'Instagram: '+C+'' + (''+P+'Unavailable' if instagram(username) else ''+P+'Available'))
-        print(W+'Medium:    '+C+'' + (''+P+'Unavailable' if medium(username) else ''+P+'Available'))
-        print(W+'Minecraft: '+C+'' + (''+P+'Unavailable' if minecraft(username) else ''+P+'Available'))
-        print(W+'Pastebin:  '+C+'' + (''+P+'Unavailable' if pastebin(username) else ''+P+'Available'))
-        print(W+'Reddit:    '+C+'' + (''+P+'Unavailable' if reddit(username) else ''+P+'Available'))
-        print(W+'TryHackMe: '+C+'' + (''+P+'Unavailable' if tryhackme(username) else ''+P+'Available'))
-        print(W+'Twitch:    '+C+'' + (''+P+'Unavailable' if twitch(username) else ''+P+'Available'))
-        print(W+'Twitter:   '+C+'' + (''+P+'Unavailable' if twitter(username) else ''+P+'Available'))
-        print(W+'YouTube:   '+C+'' + (''+P+'Unavailable' if youtube(username) else ''+P+'Available'))
-        print('\n')
-        main()
+    checker()
+
 banner()
 main()
-
