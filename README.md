719b9d4ccd5a71a77dc75b92e5371c419af83b25
kyli0x <kyli0x@protonmail.ch>
2020-11-10 20:24:08 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## Handlecheck
+A simple username checker for many popular sites.
+
+### Available websites to check:
+- Github
+- Instagram
+- Medium
+- Pastebin
+- Reddit
+- TryHackMe
+- Twitch
+- Twitter
+- YouTube
+
+## Install
+To install and use Handlecheck, you can build locally:
+```
+git clone https://github.com/kyli0x/handlecheck.git
+cd handlecheck/
+python3 handlecheck.py
+```
+
+### More websites will be added soon!
---
+++ handlecheck.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+import os
+import requests
+
+# colors
+W  = '\033[0m'  # white (default)
+P  = '\033[1;35m' # purple
+C  = '\033[1;36m' # cyan
+
+username = ''
+
+def checker(url):
+    r = requests.get(url)
+    if r.status_code == 200:
+        return True
+    return False
+
+def github(handle):
+    url = 'https://github.com/' + handle
+    return checker(url)
+
+def instagram(handle):
+    url = 'https://www.instagram.com/' + handle
+    return checker(url)
+
+def medium(handle):
+    url = 'https://medium.com/@' + handle
+    return checker(url)
+
+def pastebin(handle):
+    url = 'https://pastebin.com/u/' + handle
+    return checker(url)
+
+def reddit(handle):
+    url = 'https://reddit.com/r/' + handle
+    return checker(url)
+
+def tryhackme(handle):
+    url = 'https://tryhackme.com/p/' + handle
+    return checker(url)
+
+def twitch(handle):
+    url = 'https://twitch.tv/' + handle
+    return checker(url)
+
+def twitter(handle):
+    url = 'https://www.twitter.com/' + handle
+    return checker(url)
+
+def youtube(handle):
+    url = 'https://www.youtube.com/user/' + handle
+    return checker(url)
+
+def banner():
+    try:
+        os.system('cls')
+        raise ValueError('Error')
+    except Exception:
+        os.system('clear')
+    print ('')
+    print (C+'  Welcome to Handlecheck')
+    print ('')
+    print (C+' '+C+' Created by: Kyli0x')
+    print (P+'  GitHub: ['+C+'kyli0x'+P+']')
+    print (P+'  Insta:  ['+C+'kyli0x'+P+']')
+    print ('')
+
+def main():
+    username = input(C+' Type ['+P+'exit'+C+'] to close the program\n \n Enter a username to search:\n \n > '+W)
+    if username == 'exit':
+        exit()
+    else:
+        print('\n')
+        print(W+'Github:    '+C+'' + (''+P+'Unavailable' if github(username) else ''+P+'Available'))
+        print(W+'Instagram: '+C+'' + (''+P+'Unavailable' if instagram(username) else ''+P+'Available'))
+        print(W+'Medium:    '+C+'' + (''+P+'Unavailable' if medium(username) else ''+P+'Available'))
+        print(W+'Pastebin:  '+C+'' + (''+P+'Unavailable' if pastebin(username) else ''+P+'Available'))
+        print(W+'Reddit:    '+C+'' + (''+P+'Unavailable' if reddit(username) else ''+P+'Available'))
+        print(W+'TryHackMe: '+C+'' + (''+P+'Unavailable' if tryhackme(username) else ''+P+'Available'))
+        print(W+'Twitch:    '+C+'' + (''+P+'Unavailable' if twitch(username) else ''+P+'Available'))
+        print(W+'Twitter:   '+C+'' + (''+P+'Unavailable' if twitter(username) else ''+P+'Available'))
+        print(W+'YouTube:   '+C+'' + (''+P+'Unavailable' if youtube(username) else ''+P+'Available'))
+        print('\n')
+        main()
+banner()
+main()
+
